92ee2f10f2a150c27204d3d5aec0675e7d8a5b95
jongyoon choi <drager72@naver.com>
2023-11-15 15:17:08 +0900

    커밋

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Box from './Box';
+
+export default function App() {
+  return (
+    <main>
+      <Box name='chaos1' num='1'/>
+      <Box name='chaos2' num='2'/>
+      <Box name='chaos3' num='3'/>
+      <Box name='chaos4' num='4'/>
+    </main>
+  )
+}
---
+++ Box.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Box(props) {
+  const clickMe = ()=>{
+    alert("CLICK CLOSE   ")
+  }
+  return (
+    <div className='box'>
+      Box{props.num}
+      {props.name}
+      <button onClick={clickMe}>click!</button>
+    </div>
+  );
+}
+
+export default Box
